7da13bfe8056c8c15b23c2cf492dcde274822a67
joorani <shinerani16@gmail.com>
2022-09-23 10:02:26 +0900

    #3 - logging level 설정

--- src/main/java/com/joorani/myshop/MyShopApplication.java
+++ src/main/java/com/joorani/myshop/MyShopApplication.java
@@ -1,13 +1,16 @@
 package com.joorani.myshop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MyShopApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyShopApplication.class, args);
+    log.debug("logging test = {}", MyShopApplication.class);
   }
 
 }
