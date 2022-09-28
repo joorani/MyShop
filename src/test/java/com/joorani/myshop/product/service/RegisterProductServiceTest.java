1ba861f79f99a19491acecfe5a0d12069dd6531d
joorani <shinerani16@gmail.com>
2022-09-28 19:25:45 +0900

    Test: store에서 등록한 상품 조회

--- src/test/java/com/joorani/myshop/product/service/RegisterProductServiceTest.java
+++ src/test/java/com/joorani/myshop/product/service/RegisterProductServiceTest.java
@@ -3,10 +3,10 @@ package com.joorani.myshop.product.service;
 import com.joorani.myshop.member.domain.entity.Store;
 import com.joorani.myshop.member.domain.repository.StoreRepository;
 import com.joorani.myshop.product.domain.dtos.ProductRegisterForm;
+import com.joorani.myshop.product.domain.dtos.RegisteredProductDto;
 import com.joorani.myshop.product.domain.entity.Product;
 import com.joorani.myshop.product.domain.entity.ProductStatus;
 import com.joorani.myshop.product.repository.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,31 +14,27 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
-class ProductServiceTest {
+class RegisterProductServiceTest {
 
     @InjectMocks
-    private ProductService productService;
+    private RegisterProductService registerProductService;
 
     @Mock
     private StoreRepository storeRepository;
 
     @Mock
     private ProductRepository productRepository;
-
-
-    @BeforeEach
-    void setUp() {
-
-    }
 
     @Test
     @DisplayName("상품 등록에 성공하는 경우")
@@ -67,7 +63,7 @@ class ProductServiceTest {
         when(productRepository.save(any())).thenReturn(product);
 
         //when
-        Product savedProduct = productService.register(productRegisterForm, storeId);
+        Product savedProduct = registerProductService.register(productRegisterForm, storeId);
 
         //then
         assertThat(savedProduct.getPrice()).isEqualTo(10000);
@@ -102,12 +98,53 @@ class ProductServiceTest {
         when(productRepository.save(any())).thenReturn(product);
 
         //when
-        Product savedProduct = productService.register(productRegisterForm, storeId);
+        Product savedProduct = registerProductService.register(productRegisterForm, storeId);
 
         //then
         assertThat(savedProduct.getPrice()).isEqualTo(10000);
         assertThat(savedProduct.getProductStatus()).isEqualTo(ProductStatus.OUT_OF_STOCK);
-
     }
 
+    @Test
+    @DisplayName("Store가 등록한 전체 상품이 조회된다.")
+    void findAll() {
+        //given
+        Long storeId = 1L;
+
+        Store store = Store.builder()
+                .id(storeId)
+                .email("email@test.com")
+                .password("password")
+                .registeredNumber("12-345-677")
+                .storeName("store name")
+                .build();
+
+        Product product1 = Product.builder()
+                .name("product1")
+                .price(10000)
+                .stockQuantity(2000)
+                .imgPath("image path")
+                .store(store)
+                .build();
+
+        Product product2 = Product.builder()
+                .name("product2")
+                .price(7000)
+                .stockQuantity(3000)
+                .imgPath("image path")
+                .store(store)
+                .build();
+
+        List<Product> productList= new ArrayList<>();
+        productList.add(product1);
+        productList.add(product2);
+
+        when(storeRepository.findById(anyLong())).thenReturn(Optional.of(store));
+        when(productRepository.findAllByStore(any())).thenReturn(productList);
+
+        List<RegisteredProductDto> registeredProductDtos = registerProductService.findAllProducts(storeId);
+
+        assertThat(registeredProductDtos.get(0).getProductName()).isEqualTo(product1.getName());
+        assertThat(registeredProductDtos.size()).isEqualTo(2);
+    }
 }
