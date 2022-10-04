e90ef6c50040a9df0efa7af1178f29ed3c7d6d4a
joorani <shinerani16@gmail.com>
2022-10-04 17:11:36 +0900

    Feature: product에 Option entity 추가

--- src/main/java/com/joorani/myshop/entity/Product.java
+++ src/main/java/com/joorani/myshop/entity/Product.java
@@ -4,25 +4,24 @@ import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 public class Product extends BaseEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column(name = "product_id")
     private Long id;
 
-    @Column(unique = true, nullable = false, length = 20)
+    @Column(nullable = false, length = 20)
     private String name;
 
-    @Column(unique = true, nullable = false)
-    private int stockQuantity;
-
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private int price;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String imgPath;
 
     @Enumerated(value = EnumType.STRING)
@@ -33,22 +32,33 @@ public class Product extends BaseEntity{
     @JoinColumn(name = "store_id")
     private Store store;
 
+    // 상품 카테고리
+    @Embedded
+    private ProductCategory productCategory;
+
+    // 상품 옵션
+    @OneToMany(mappedBy = "product") //조회만 가능
+    private List<ProductOption> options = new ArrayList<>();
+
     protected Product() {
+
     }
 
     @Builder
-    public Product(Long id, String name, int stockQuantity, int price, String imgPath, Store store) {
-        this.id = id;
+    public Product(String name, int price, String imgPath, ProductStatus productStatus, Store store, ProductCategory productCategory, List<ProductOption> options) {
         this.name = name;
-        this.stockQuantity = stockQuantity;
         this.price = price;
         this.imgPath = imgPath;
-        this.productStatus = setProductStatus();
+        this.productStatus = options.size() > 0 ? ProductStatus.ON_SALE : ProductStatus.OUT_OF_STOCK;
         this.store = store;
+        this.productCategory = productCategory;
+        this.options = options;
     }
 
-    public ProductStatus setProductStatus() {
-        return productStatus = (this.stockQuantity == 0) ? ProductStatus.OUT_OF_STOCK : ProductStatus.ON_SALE;
+    public void addStore(Store store) {
+        this.store = store;
+        if (!store.getProducts().contains(this)) {
+            store.getProducts().add(this);
+        }
     }
-
 }
