725f2549e96ef0535b14e82aae97dc719011dec5
joorani <shinerani16@gmail.com>
2022-10-31 18:25:01 +0900

    [#16]Docs: ReviewController 리뷰등록 swagger 수정

--- src/main/java/com/joorani/myshop/controller/ReviewController.java
+++ src/main/java/com/joorani/myshop/controller/ReviewController.java
@@ -1,10 +1,13 @@
 package com.joorani.myshop.controller;
 
-import com.joorani.myshop.entity.dtos.ReviewSaveRequestDto;
+import com.joorani.myshop.controller.dtos.ReviewSaveRequestDto;
 import com.joorani.myshop.service.ReviewService;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,11 +28,18 @@ public class ReviewController {
     })
     @PostMapping("/reviews")
     public void register(
+            @Parameter(description = "주문번호", required = true, example = "123")
             @RequestParam("orderNo") Long orderNo,
+            @Parameter(description = "상품번호", required = true, example = "456")
             @RequestParam("productId") Long productId,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "리뷰 작성", required = true,
+                    content = @Content(schema = @Schema(implementation = ReviewSaveRequestDto.class)))
             @RequestBody ReviewSaveRequestDto reviewSaveRequestDto
     ) {
+
         reviewService.registerReview(orderNo, productId, reviewSaveRequestDto);
     }
+
 
 }
