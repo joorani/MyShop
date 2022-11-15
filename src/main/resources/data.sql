d8c215ce420284ac762bd7bd9ba80384b1a068c7
joorani <shinerani16@gmail.com>
2022-11-15 12:30:47 +0900

    [#16]Feature: 리뷰 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO PRODUCT(name, price, img_path) VALUES ('상품1', 10000, 'img path1');
+
+-- 리뷰 --
+INSERT INTO REVIEW(created_at, modified_at, content, author, product_id, product_name)
+VALUES ('20221103T19:18:40.716822', '2022-11-03T19:18:40.716822', '1번 리뷰 내용입니다.', '작성자1', 1, '싱품1');
+
+INSERT INTO REVIEW(created_at, modified_at, content, author, product_id, product_name)
+VALUES ('2022-11-03T19:18:40.716822', '2022-11-03T19:18:40.716822', '2번 리뷰 내용입니다.', '작성자1', 1, '싱품1');
+
+INSERT INTO REVIEW(created_at, modified_at, content, author, product_id, product_name)
+VALUES ('2022-11-03T19:18:40.716822', '2022-11-03T19:18:40.716822', '3번 리뷰 내용입니다.', '작성자1', 1, '싱품1');
+
+INSERT INTO REVIEW(created_at, modified_at, content, author, product_id, product_name)
+VALUES ('2022-11-03T19:18:40.716822', '2022-11-03T19:18:40.716822', '4번 리뷰 내용입니다.', '작성자1', 1, '싱품1');
+
+INSERT INTO REVIEW(created_at, modified_at, content, author, product_id, product_name)
+VALUES ('2022-11-03T19:18:40.716822', '2022-11-03T19:18:40.716822', '5번 리뷰 내용입니다.', '작성자1', 1, '싱품1');
